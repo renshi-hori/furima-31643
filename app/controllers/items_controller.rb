2f8c268e2696a3f1b6fb429e25b906419c4456b9
renshi-hori <renshi0715@icloud.com>
2020-11-30 17:21:49 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update, :destory]
+
 
   def index
     @items = Item.all.order("created_at ASC")
@@ -19,15 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
      redirect_to item_path
     else
@@ -36,10 +38,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
+    
     if current_user.id == @item.user_id 
-    @item.destroy
-    redirect_to root_path
+     @item.destroy
+     redirect_to root_path
     else
       redirect_to root_path
     end
@@ -53,5 +55,8 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:name, :category_id, :sales_status_id, :info, :shipping_free_status_id, :prefecture_id, :scheduled_delivert_id, :price, :image).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
