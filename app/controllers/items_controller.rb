5a402ac53219bee94760583a6dbc5c71c3acc764
renshi-hori <renshi0715@icloud.com>
2020-12-06 16:56:55 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update, :destory]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
   def index
