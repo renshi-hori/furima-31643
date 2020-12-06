ca9db97ceee6f0ae129e9f9115716491159fa9cd
renshi-hori <renshi0715@icloud.com>
2020-12-06 16:42:29 +0900

    修正１

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_address_params)
+  
     if @order_address.valid?
       pay_item
       
@@ -16,6 +17,7 @@ class OrdersController < ApplicationController
     redirect_to root_path
     else
     render :index
+    
     end
   end
 
